7d7cc2987995352eb0dd7597e234162006db38ab
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-07-27 22:30:20 -0300

    configure the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
 
 function App() {
-  return <h1>Teste</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
