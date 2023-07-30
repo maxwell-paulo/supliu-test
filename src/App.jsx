ed6daee92e528cd12c85ebce27d04f167fe39f70
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-07-30 13:21:10 -0300

    install and configure styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, CreateAlbum } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create-album" element={<CreateAlbum />} />
       </Routes>
     </BrowserRouter>
   );
