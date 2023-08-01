3b0984c36a040e1c52d035a4733d339ad26f0eff
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-07-31 22:35:32 -0300

    import create track page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, CreateAlbum, Album } from "./pages";
+import { Home, CreateAlbum, Album, CreateTrack } from "./pages";
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-album" element={<CreateAlbum />} />
+        <Route path="/add-track/:id" element={<CreateTrack />} />
         <Route path="/serched-album" element={<Album />} />
       </Routes>
     </BrowserRouter>
